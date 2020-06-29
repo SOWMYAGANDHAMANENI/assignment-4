b81bd5377af1a676fa3f62eb480cabbe04077c2e
SOWMYAGANDHAMANENI <67108862+SOWMYAGANDHAMANENI@users.noreply.github.com>
2020-06-29 17:25:13 +0530

    Add files via upload
    G.Sowmya

---
+++ maxno.py
@@ -0,0 +1,8 @@
+a = int(input("enter the first number"))
+b= int(input("enter the second number"))
+c = int(input("enter the third number"))
+A = a>=b and a>=c	
+print(" a is larger :",A)
+B = b>=a and b>=c
+print(" b is larger:",B)
+print (" c is larger")
